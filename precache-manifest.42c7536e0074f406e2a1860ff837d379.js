b197ec3ecaab1762e05887002b67c5403e589379
Phyllis Kan <pyk9q7@gmail.com>
2020-11-08 13:09:33 -0600

    Updates

---
+++ precache-manifest.42c7536e0074f406e2a1860ff837d379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a64c6c539d743c8c26845bd10a4c28e",
+    "url": "/pk-portfolio/index.html"
+  },
+  {
+    "revision": "60e54757a51fa83e5476",
+    "url": "/pk-portfolio/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2e7a3e7fdb937e49b274",
+    "url": "/pk-portfolio/static/js/2.386a4247.chunk.js"
+  },
+  {
+    "revision": "60e54757a51fa83e5476",
+    "url": "/pk-portfolio/static/js/main.c0fead12.chunk.js"
+  },
+  {
+    "revision": "f918dcec64c53a638471",
+    "url": "/pk-portfolio/static/js/runtime~main.bf22f664.js"
+  }
+]);
